d0b4608eba9a2db72aa8b67d2095e9a4b75653b9
Hasnat4763 <abulhasnat5737@gmail.com>
2025-08-14 18:23:40 +0600

    Adding New block

---
+++ src/main/java/net/hasnat4763/block/ModBlocks.java
@@ -0,0 +1,59 @@
+package net.hasnat4763.block;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.hasnat4763.Nusiam;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
+
+public class ModBlocks {
+
+    public static final Block NUSIAM_ORE = registerBlock("nusiam_ore", Block::new,
+            AbstractBlock.Settings.create().strength(4f).requiresTool().sounds(BlockSoundGroup.COPPER),
+            true);
+
+    private static Block registerBlock(String name, Function<AbstractBlock.Settings, Block> blockFactory, AbstractBlock.Settings settings, boolean shouldRegisterItem) {
+        // Create a registry key for the block
+        RegistryKey<Block> blockKey = keyOfBlock(name);
+        // Create the block instance
+        Block block = blockFactory.apply(settings.registryKey(blockKey));
+
+        // Sometimes, you may not want to register an item for the block.
+        // Eg: if it's a technical block like `minecraft:moving_piston` or `minecraft:end_gateway`
+        if (shouldRegisterItem) {
+            // Items need to be registered with a different type of registry key, but the ID
+            // can be the same.
+            RegistryKey<Item> itemKey = keyOfItem(name);
+
+            BlockItem blockItem = new BlockItem(block, new Item.Settings().registryKey(itemKey));
+            Registry.register(Registries.ITEM, itemKey, blockItem);
+        }
+
+        return Registry.register(Registries.BLOCK, blockKey, block);
+    }
+
+    private static RegistryKey<Block> keyOfBlock(String name) {
+        return RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(Nusiam.MOD_ID, name));
+    }
+
+    private static RegistryKey<Item> keyOfItem(String name) {
+        return RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Nusiam.MOD_ID, name));
+    }
+
+    public static void registerModBlocks() {
+        Nusiam.LOGGER.info("Registering blocks for" + Nusiam.MOD_ID);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(entries->{
+            entries.add(NUSIAM_ORE);
+        });
+    }
+}
